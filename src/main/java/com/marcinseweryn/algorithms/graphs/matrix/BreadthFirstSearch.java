7d09f79f136ad08943cd99e2e87df2bf759427c2
Marcin Seweryn <marcinsewerynn@gmail.com>
2023-04-19 15:50:44 +0200

    Refactor of adjMatrix BreadtFirstSearch and test added

--- src/main/java/com/marcinseweryn/algorithms/graphs/matrix/BreadthFirstSearch.java
+++ src/main/java/com/marcinseweryn/algorithms/graphs/matrix/BreadthFirstSearch.java
@@ -3,39 +3,72 @@ package com.marcinseweryn.algorithms.graphs.matrix;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
+/**
+ * Implements a Breadth-First Search algorithm on a graph represented as an adjacency matrix.
+ */
 public class BreadthFirstSearch {
     int[][] adjacencyMatrix;
 
+    /**
+     * Constructs a new BreadthFirstSearch object with the given adjacency matrix.
+     *
+     * @param adjacencyMatrix the adjacency matrix representing the graph
+     */
     public BreadthFirstSearch(int[][] adjacencyMatrix) {
         this.adjacencyMatrix = adjacencyMatrix;
     }
 
-    public void printBreadthFirstSearch(int start) {
+    /**
+     * Performs a Breadth-First Search on the graph starting from the given vertex,
+     * and return visited vertices.
+     *
+     * @param start the starting vertex for the search
+     * @return String representation of executed breadth first search algorithm on the graph
+     */
+    public String printBreadthFirstSearch(int start) {
+        StringBuilder bfs = new StringBuilder();
+        bfs.append(
+                String.format("Breadth First Search from vertex %d:[", start)
+        );
+
+        // Create a boolean array to keep track of visited vertices
         boolean[] visited = new boolean[adjacencyMatrix.length];
+
+        // Create a Queue to keep track of vertices to be visited
         Queue<Integer> queue = new ArrayDeque<>();
+
+        // Add the starting vertex to the queue and mark it as visited
         queue.add(start);
         visited[start] = true;
-        while (!queue.isEmpty()) {
-            int current = queue.remove();
-            System.out.print(current + " ");
 
-            // neighbors
+        // Continue until all vertices have been visited
+        while (!queue.isEmpty()) {
+
+            // Remove the next vertex from the queue and print it
+            int current = queue.remove();
+            bfs.append(current).append(" ");
+
+            // Visit all neighbors of the current vertex
             for (int j = 0; j < adjacencyMatrix[current].length; j++) {
-                // int start has a neighbor
+
+                // If the neighbor is adjacent and hasn't been visited yet,
+                // add it to the queue and mark it as visited
                 if (adjacencyMatrix[current][j] == 1 && (!visited[j])) {
-                        queue.add(j);
-                        visited[j] = true;
+                    queue.add(j);
+                    visited[j] = true;
                 }
             }
         }
+        bfs.replace(bfs.length() - 1, bfs.length(), "]");
+        return bfs.toString();
     }
 
     public static void main(String[] args) {
-        // A -- B
+        // 0 -- 1
         // | \    \
-        // |  \    E
+        // |  \    4
         // |   \  /
-        // C -- D
+        // 2 -- 3
         int[][] matrix = new int[][]{
                 {0, 1, 1, 1, 0},
                 {1, 0, 0, 0, 1},
@@ -44,8 +77,7 @@ public class BreadthFirstSearch {
                 {0, 1, 0, 1, 0}
         };
         BreadthFirstSearch bfs = new BreadthFirstSearch(matrix);
-        bfs.printBreadthFirstSearch(0);
-        System.out.println();
+        System.out.println(bfs.printBreadthFirstSearch(0));
 
         //                 1     12
         //               /   \ /   \
@@ -73,10 +105,11 @@ public class BreadthFirstSearch {
         };
 
         bfs = new BreadthFirstSearch(matrix);
-        bfs.printBreadthFirstSearch(0);
+        System.out.println(bfs.printBreadthFirstSearch(0));
 
-        // Print:
-        //0 1 2 3 4
-        //0 7 9 11 3 6 8 10 4 5 1 12 2
+        // Output:
+        //Breadth First Search from vertex 0:[0 1 2 3 4]
+        //Breadth First Search from vertex 0:[0 7 9 11 3 6 8 10 4 5 1 12 2]
+
     }
 }
---
+++ src/test/java/com/marcinseweryn/algorithms/graphs/matrix/BreadthFirstSearchTest.java
@@ -0,0 +1,60 @@
+package com.marcinseweryn.algorithms.graphs.matrix;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BreadthFirstSearchTest {
+
+    @ParameterizedTest
+    @MethodSource("graphProvider")
+    public void testPrintBreadthFirstSearch(int[][] adjacencyMatrix, int start, String expected) {
+        BreadthFirstSearch bfs = new BreadthFirstSearch(adjacencyMatrix);
+        String actual = bfs.printBreadthFirstSearch(start);
+        assertEquals(expected, actual);
+    }
+
+    static Collection<Object[]> graphProvider() {
+        return Arrays.asList(new Object[][] {
+
+                // 0 --- 1
+                //   |     |
+                //   3 --- 2
+                // Graph 1
+                { new int[][] {
+                        {0, 1, 1, 0},
+                        {1, 0, 1, 1},
+                        {1, 1, 0, 1},
+                        {0, 1, 1, 0}
+                }, 0, "Breadth First Search from vertex 0:[0 1 2 3]" },
+
+                //  0 --- 1
+                //    \   /
+                //     \ /
+                //      2
+                // Graph 2
+                { new int[][] {
+                        {0, 1, 1},
+                        {1, 0, 1},
+                        {1, 1, 0}
+                }, 1, "Breadth First Search from vertex 1:[1 0 2]" },
+
+                //   0 --- 1
+                //    \   / \
+                //     \ /   |
+                //      2 --- 3
+                // Graph 3
+                { new int[][] {
+                        {0, 1, 0, 0},
+                        {1, 0, 1, 1},
+                        {0, 1, 0, 1},
+                        {0, 1, 1, 0}
+                }, 3, "Breadth First Search from vertex 3:[3 1 2 0]" },
+
+        });
+    }
+}
